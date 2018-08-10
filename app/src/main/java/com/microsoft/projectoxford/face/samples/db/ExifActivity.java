6046ddf44738dcea1ef7293457a370fea73439ca
jieun <skskje312@gmail.com>
2018-08-11 00:44:16 +0900

    사진 정보2(위도,경도값으로 지역명 구하기.)

--- app/src/main/java/com/microsoft/projectoxford/face/samples/db/ExifActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/face/samples/db/ExifActivity.java
@@ -2,11 +2,14 @@ package com.microsoft.projectoxford.face.samples.db;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,14 +23,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+/*exif정보가 있는 사진만 분석을 할 수 있는데.. 우리 앱 카메라는 사진 찍어도 사진 안에 exif정보가 없어 ㅠ
+* exif정보 넣는거 찾아봐야 할 것 같아 ! 그리고 snow같은 카메라들은 높이,너비 값만 exif정보가 있고 gps정보는 없어. */
 
 public class ExifActivity extends AppCompatActivity {
 
     TextView textView;
     ImageView imageView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exif);
+        textView = (TextView)findViewById(R.id.textView);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,21 +66,6 @@ public class ExifActivity extends AppCompatActivity {
         } }catch (Exception e){
                 Toast.makeText(getApplicationContext(),"오류",Toast.LENGTH_LONG).show();
         }}
-
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exif);
-        textView = (TextView)findViewById(R.id.textView);
-        imageView = (ImageView)findViewById(R.id.imageView);
-
-
-    }
-
-
 
     public void onClickgallery(View view) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -139,7 +140,12 @@ public class ExifActivity extends AppCompatActivity {
                 }
                 stringBuffer.append("\n");
             }
-            stringBuffer.append("\n[End information] \n" + attributes.length + "");
+            Float Latitude = convertToDegree(exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE)); // 위도
+            Float Longitude = convertToDegree(exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)); //경도
+            stringBuffer.append("위도 : "+Latitude+", 경도 : "+Longitude);
+            stringBuffer.append("\n[지역명] \n"+location(Latitude,Longitude));
+            stringBuffer.append("\n[End information]");
+
             textView.setText(stringBuffer.toString());
 
         } catch (FileNotFoundException e) {
@@ -150,4 +156,69 @@ public class ExifActivity extends AppCompatActivity {
 
     }
 
+    private Float convertToDegree(String stringDMS){
+        Float result = null;
+        String[] DMS = stringDMS.split(",", 3);
+
+        String[] stringD = DMS[0].split("/", 2);
+        Double D0 = new Double(stringD[0]);
+        Double D1 = new Double(stringD[1]);
+        Double FloatD = D0/D1;
+
+        String[] stringM = DMS[1].split("/", 2);
+        Double M0 = new Double(stringM[0]);
+        Double M1 = new Double(stringM[1]);
+        Double FloatM = M0/M1;
+
+        String[] stringS = DMS[2].split("/", 2);
+        Double S0 = new Double(stringS[0]);
+        Double S1 = new Double(stringS[1]);
+        Double FloatS = S0/S1;
+
+        result = new Float(FloatD + (FloatM/60) + (FloatS/3600));
+
+        return result;
+
+
+    };
+
+
+    public String location(Float Latitude,Float Longitude ){
+        final Geocoder geocoder = new Geocoder(this);
+        List<Address> list = null;
+        try {
+            double d1 = Double.parseDouble(String.valueOf(Latitude));
+            double d2 = Double.parseDouble(String.valueOf(Longitude));
+
+            list = geocoder.getFromLocation(
+                    d1, // 위도
+                    d2, // 경도
+                    10); // 얻어올 값의 개수
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러발생");
+        }
+        if (list != null) {
+            if (list.size()==0) {
+                return "해당되는 주소 정보는 없습니다";
+            } else {
+                // 구는 null값이 나오는 곳이 있고 안나오는 곳이있다.
+                // 시까지는 정확한 결과인것 같다.
+                // 핸드폰 자체에서도 매번 정확한 gps를 정보를 가져올 수는 없는 것같다. 핸드폰 기본 갤러리에서도 내가 실제 찍은 장소랑 살짝 오차가 있음.
+                // 번지까지 정보를 가져오기에는 너무 오차가능성이 높아질 듯.
+
+                StringBuffer stringBuffer= new StringBuffer();
+                stringBuffer.append(list.get(0).getCountryName()+" ");//국가명
+                stringBuffer.append(list.get(0).getLocality()+" ");//구 메인(시)
+                stringBuffer.append(list.get(0).getSubLocality()+" ");//구 서브데이터
+                stringBuffer.append(list.get(0).getThoroughfare()+" ");//동
+                //stringBuffer.append(list.get(0).getSubThoroughfare());//번지
+
+
+                return stringBuffer.toString();
+                        // list.get(0).getAddressLine(0).toString(); // 전체주소(국가, 시, 구, 동, 번지)
+            }
+        }
+        return "";
+    }
 }
